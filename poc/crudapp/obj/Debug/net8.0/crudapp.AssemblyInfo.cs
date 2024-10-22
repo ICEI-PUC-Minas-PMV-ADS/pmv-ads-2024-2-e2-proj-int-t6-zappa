8110fae483c5059a8fc2361bb1f1954458c1b0e7
AndreDexheimer <andre.dexheimer@gmail.com>
2024-10-21 23:10:02 -0300

    teste

--- poc/crudapp/obj/Debug/net8.0/crudapp.AssemblyInfo.cs
+++ poc/crudapp/obj/Debug/net8.0/crudapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crudapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0ba671474054cac3956988283c1e48b3a62b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("crudapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crudapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
