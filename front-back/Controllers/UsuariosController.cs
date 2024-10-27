0f3716d1ed9c4bc277435555baa6489e17dd83f8
AndreDexheimer <andre.dexheimer@gmail.com>
2024-10-26 22:18:24 -0300

    primeiro push com o back do create user pronto

---
+++ front-back/Controllers/UsuariosController.cs
@@ -0,0 +1,37 @@
+﻿using front_back.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace front_back.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public UsuariosController(AppDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index() 
+        {
+            var dados = await _context.Usuarios.ToListAsync();
+
+            return View(dados);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+            }
+            return View(usuario);
+        }
+    }
+
+}
--- front-back/Program.cs
+++ front-back/Program.cs
@@ -1,9 +1,14 @@
+using front_back.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
