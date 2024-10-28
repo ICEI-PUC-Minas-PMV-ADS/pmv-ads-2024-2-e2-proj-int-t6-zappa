15342544e01f8056bcf73ba791d0a06fdb29643b
AndreDexheimer <andre.dexheimer@gmail.com>
2024-10-28 18:25:23 -0300

    Criação do banco de dados de alimentos

--- front-back/Models/AppDbContext.cs
+++ front-back/Models/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace front_back.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Alimento> Alimentos { get; set; }
 
     }
 }
