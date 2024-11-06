9653453aa06d1e9c9c65e18d30b39a7e06fa5685
AndreDexheimer <andre.dexheimer@gmail.com>
2024-11-05 21:39:43 -0300

    ajustes no cadastro.
    falta a mensagem de sucesso.

--- front-back/Controllers/UsuariosController.cs
+++ front-back/Controllers/UsuariosController.cs
@@ -3,19 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol;
 
+
 namespace front_back.Controllers
 {
     public class UsuariosController : Controller
     {
         private readonly AppDbContext _context;
-        public UsuariosController(AppDbContext context) 
+
+        public UsuariosController(AppDbContext context)
         {
             _context = context;
         }
-        public async Task<IActionResult> Index() 
+
+        public async Task<IActionResult> Index()
         {
             var dados = await _context.Usuarios.ToListAsync();
-
             return View(dados);
         }
 
@@ -23,26 +25,34 @@ namespace front_back.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Create(Usuario usuario)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) 
             {
                 _context.Add(usuario);
                 await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Cadastro realizado com sucesso!";
+
+                // Redireciona para a lista de usuários após o cadastro
+                return View();
             }
+            // Se o modelo não for válido, retorna a mesma view com os erros
             return View(usuario);
         }
+
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null) 
-                    return NotFound();
+            if (id == null)
+                return NotFound();
             var dados = await _context.Usuarios.FindAsync(id);
             if (dados == null)
                 return NotFound();
 
-            return View();
+            return View(dados);
         }
+
         [HttpPost]
         public async Task<IActionResult> Edit(int id, Usuario usuario)
         {
@@ -52,9 +62,9 @@ namespace front_back.Controllers
             {
                 _context.Usuarios.Update(usuario);
                 await _context.SaveChangesAsync();
+                return RedirectToAction("Index"); // Redirecionar após edição
             }
-            return View();
+            return View(usuario); // Retorna a view com erros
         }
     }
-
 }
