1fc50b6293339a9ccc66427eebd03ef646d06031
AndreDexheimer <andre.dexheimer@gmail.com>
2024-11-22 20:31:21 -0300

    - Novos atributos na tabela aliimentos - Novos atributios no form create alimentos

--- zappa-web-app/front-back/Migrations/20241122225407_AddNewAttributesToAlimentos.Designer.cs
+++ zappa-web-app/front-back/Migrations/20241122225407_AddNewAttributesToAlimentos.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using front_back.Models;
 
@@ -11,9 +12,11 @@ using front_back.Models;
 namespace front_back.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241122225407_AddNewAttributesToAlimentos")]
+    partial class AddNewAttributesToAlimentos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ zappa-web-app/front-back/Migrations/20241122225407_AddNewAttributesToAlimentos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace front_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewAttributesToAlimentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Consumo",
+                table: "Alimentos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DespensaId",
+                table: "Alimentos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Validade",
+                table: "Alimentos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Consumo",
+                table: "Alimentos");
+
+            migrationBuilder.DropColumn(
+                name: "DespensaId",
+                table: "Alimentos");
+
+            migrationBuilder.DropColumn(
+                name: "Validade",
+                table: "Alimentos");
+        }
+    }
+}
